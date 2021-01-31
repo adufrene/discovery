3c52e71cea1ec608cbf83d87dc48c620b5b35c70
Andy DuFrene <andy@getclockwise.com>
2021-01-31 14:42:11 -0800

    chapter 5

--- src/05-led-roulette/src/main.rs
+++ src/05-led-roulette/src/main.rs
@@ -2,14 +2,29 @@
 #![no_main]
 #![no_std]
 
-use aux5::entry;
+use aux5::{entry, prelude::*};
+
+const NUM_LEDS: usize = 8;
+const DELAY_DURATION: u16 = 50;
+
 
 #[entry]
 fn main() -> ! {
-    let _y;
-    let x = 42;
-    _y = x;
+    let (mut delay, mut leds) = aux5::init();
 
-    // infinite loop; just so we don't leave this stack frame
-    loop {}
+    loop {
+        for current_led_index in 0..NUM_LEDS {
+            let next_led_index = next_led(current_led_index);
+
+            leds[next_led_index].on();
+            delay.delay_ms(DELAY_DURATION);
+
+            leds[current_led_index].off();
+            delay.delay_ms(DELAY_DURATION);
+        }
+    }
+}
+
+fn next_led(previous_led_index: usize) -> usize {
+    (previous_led_index + 1) % NUM_LEDS
 }
